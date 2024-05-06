fa3330f08b120167f6f1fc612f480a8157520084
nathan-myers <158767745+nathan-myers@users.noreply.github.com>
2024-05-06 05:52:19 +0530

    Update Practical 13.ino

--- Practical 13.ino
+++ Practical 13.ino
@@ -1,10 +1,13 @@
 void setup() 
 { 
-  // put your setup code here, to run once: pinMode(10, OUTPUT);
+  // put your setup code here, to run once: 
+  pinMode(10, OUTPUT);
 } 
 
 void loop() 
-{ // put your main code here, to run repeatedly: tone(10,261); //Middle C delay(1000);
+{ // put your main code here, to run repeatedly: 
+  tone(10,261); //Middle C 
+  delay(1000);
   tone(10,277); //C# 
   delay(1000);
   tone(10,294); //D delay(1000);
